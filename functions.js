0bf4e3b003839d8d1f338d92cf1deb84f6ff4cf2
Cilek <119878525+esmagulozarsln@users.noreply.github.com>
2023-10-19 21:58:02 +0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,12 @@
+var sayi1 = Math.random()
+var sayi2 = 16 
+var sayi3 = 3 
+var sayi4 = 48
+var sayi5 = 49
+var sayi6 = 11
+
+console.log('Kolon : ' +sayi1+' '
++ sayi2+' '+sayi3+' '+sayi4+' '+sayi5+' '+sayi6+' ')
+
+
+
